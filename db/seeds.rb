3739916dddc4a18c531ba18b67fca48e38374cd8
gotqn <george_27@abv.bg>
2014-07-04 00:21:43 +0300

    Initial commit.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# Inserting default security users
+users = {
+
+    admin: {
+
+        username: 'admin',
+        email: 'tuvarna.system.master@gmail.com',
+        password: 'adminpass',
+        password_confirmation: 'adminpass',
+        is_admin: true
+    },
+
+    administrator: {
+
+        username: 'administrator',
+        email: 'tuvarna.system.super.user@gmail.com',
+        password: 'administrator',
+        password_confirmation: 'administrator',
+        is_admin: true
+    }
+}
+
+users.each do |user, data|
+
+  user = User.new(data)
+
+  unless User.where(email: user.email).exists?
+    user.save!
+  end
+end
